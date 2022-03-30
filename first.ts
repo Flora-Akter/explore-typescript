11d57b2c38375abd2b4f7632c9e64d8252bc037d
Flora-Akter <soniaflora124@gmail.com>
2022-03-30 10:47:42 +0600

    first commit

---
+++ first.ts
@@ -0,0 +1,7 @@
+// const myNumber = 5;
+// const myName = "Flora";
+// console.log("Hello Typescript", myNumber, myName);
+
+let money: number = 5;
+let honey: string =  "money is not available";
+let funny:boolean = false;
---
+++ js/first.js
@@ -0,0 +1,4 @@
+"use strict";
+const myNumber = 5;
+const myName = "Flora";
+console.log("Hello Typescript", myNumber, myName);
